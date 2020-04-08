9c7edeba9c327c41c492ccdec0e78ddd27e154f7
Ricky <rchu6120@gmail.com>
2020-04-07 23:45:50 -0400

    new progress

---
+++ hangman.py
@@ -0,0 +1,91 @@
+##### Every program starts with variables: 
+
+    # the hangman word to guess
+    # what the user has guessed so far
+    # max # of attempts
+
+#### Next you'll need a loop to keep asking the user for their guess
+
+    # The game should keep running as long as there are attempts remaining
+    # At each iteration of the loop, it should prompt the user to guess a letter
+    # If the user guesses it right, the blanks should be replaced by what the user guessed
+    # If the user guesses it wrong, it should print what the user has guessed so far along with the remaining guesses
+
+#### Your program should end when:
+
+    # the user has guessed all of the blanks
+    # the user runs out of guesses, and it should print game over
+
+#### What you'll need:
+
+# 1) A string containing the original phrase
+
+# 2) A dictionary storing the letters of the hangman phrase
+    # It should have this format: position: {letter: status}
+        # POSITION of the letter
+        # the LETTER itself
+        # STATUS of the letter: guessed/not guessed
+
+    # This is what the generated dictionary looks like. Notice how it has a NESTED structure.
+    # guessed_word = {
+    #     0: {'H': False}, 
+    #     1: {'E': False}, 
+    #     2: {'L': False}, 
+    #     3: {'L': False}, 
+    #     4: {'O': False}, 
+    # }
+
+# 3) An integer to keep track of the remaining guesses
+
+########### IMPORTANT: YOUR PROGRAM MUST WORK NO MATTER WHAT THE HANGMAN WORD IS ###########
+
+#### code here ####
+
+word_to_guess = "RICKY IS THE GREATEST"
+guessed_characters = {}
+number_of_attempts = 6
+
+for i, char in enumerate(word_to_guess):
+    guessed_characters[i] = {char: False}
+
+while number_of_attempts > 0:
+
+    #Ask the user for an input
+    guess = input("Guess a character \n")
+
+    # Iterates through the FIRST level of the dictionary
+    for i, char_info in guessed_characters.items():
+        # i is the index (position) of the character
+        # char_info is the letter and the status of it: {'H': False}
+        
+        # Iterates through the SECOND level of the dictionary
+        for char, status in char_info.items():
+            # If user guesses the letter correctly
+            if guess.upper() == char:
+                guessed_characters[i][char] = True
+                print(char, end = " ")
+            # If user guesses the letter INCORRECTLY
+            elif guess != char:
+                print("_")
+
+    # If the user guesses the letter incorrectly 
+    number_of_attempts -= 1
+
+    if number_of_attempts == 0:
+        # Print final result
+        print(guessed_characters)
+    else:
+        # Print how many guesses they have remaining 
+        print(number_of_attempts)
+
+    # If there are no more "False" in the dictionary
+        # Let the user know that they won the game            
+
+                    
+
+    
+
+    
+        
+    
+
--- review.py
+++ review.py
@@ -7,7 +7,7 @@
 # Michael purchased a pair of sneakers for $500
 # Andrew purchased a t-shirt for $200 AND a sweater for $300
 
-# Organizing the transactions by person
+# Method 1: Organizing the transactions by person
 transactions_by_person = {
     "Alex": {
         "sweater": 300
@@ -31,7 +31,7 @@ transactions_by_person = {
     }
 }
 
-# Organizing the transactions by product
+# Method 2: Organizing the transactions by product
 transactions_by_product = {
     "sweater": {
         "Alex": 300,
@@ -49,13 +49,76 @@ transactions_by_product = {
     }
 }
 
+# Method 3: Organizing the transactions by product without repeating the price
+transactions_by_product2 = {
+    "sweater": {
+        300: ["Alex", "Henry", "Andrew"]
+    },
+    "t-shirt": {
+        200: ["Jason", "Andrew"]
+    },
+    "sneakers": {
+        500: ["Kevin", "Michael", "Henry"]
+    }
+}
+
+print(transactions_by_product2)
+
+# Method 4: Organizing the transactions by purchase and price in separate dictionaries
+price = {
+    "sweater": 300,
+    "t-shirt": 200,
+    "sneakers": 500
+}
+
+purchases_by_item = {
+    "sweater": ["Alex", "Henry", "Andrew"],
+    "t-shirt": ["Jason", "Andrew"],
+    "sneakers": ["Kevin", "Michael", "Henry"]
+}
+
+purchases_by_person = {
+    "Alex": {
+        "sweater"
+    },
+    "Henry": {
+        "sweater",
+        "sneakers"
+    },
+    "Michael": {
+        "sneakers"
+    },
+    "Andrew": {
+        "sweater",
+        "t-shirt"
+    },
+    "Jason": {
+        "t-shirt"
+    }
+}
+
+print(price)
+print(purchases_by_item)
+print(purchases_by_person)
+
 
 ### 2) Print all of Andrew's purchases along with the price ***
 # Code here 
 
-for person, purchase in transactions_by_person.items():
-    if person == "Andrew":
-        print(transactions_by_person[person])
+# Solution 1: double for loops 
+for product, order in transactions_by_product.items():
+    for person, price in order.items():
+        if person == "Andrew":
+            print("Andrew purchased a " + product + " for $" + str(price))
+
+# Solution 2: using "in"
+for product, order in transactions_by_product.items():
+    if "Andrew" in order:
+        print("Andrew purchased a " + product + " for $" + str(order["Andrew"]))
+
+# Solution 3: Static/inefficient solution
+print("Andrew purchased a sweater for $" + str(transactions_by_person["Andrew"]["sweater"]))
+print("Andrew purchased a t-shirt for $" + str(transactions_by_person["Andrew"]["t-shirt"]))
 
 
 ### 3) Print all of sweater purchases along with the price using ***transactions_by_person***
@@ -65,7 +128,7 @@ for person, purchase in transactions_by_person.items():
 for person, purchase in transactions_by_person.items():
     for item, price in purchase.items():
         if item == "sweater":
-            print(item, price)
+            print(person + " purchased a " + item + " for $" + str(price))
 
 
 ### 4). Insert the string "world" after every "hello" (regardless of upper/lower casing) using a FOR loop
@@ -76,9 +139,9 @@ sentence = ["Hello", "my", "name", "is", "Jun.", "I", "like", "to", "say", "hell
 
 # Code here
 
-for count, word in enumerate(sentence):
+for i, word in enumerate(sentence):
     if "HELLO" in word.upper():
-        sentence.insert(count + 1, "world")
+        sentence.insert(i + 1, "world")
 
 print(sentence)
 
