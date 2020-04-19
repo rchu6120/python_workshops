a39339c1cf9fda9f8215d1f5a7f72b444bd54d6d
Ricky Chu <rchu6120@gmail.com>
2020-04-19 02:23:05 -0400

    fdfdf

--- hangman.py
+++ hangman.py
@@ -81,11 +81,3 @@ while number_of_attempts > 0:
     # If there are no more "False" in the dictionary
         # Let the user know that they won the game            
     
-                    
-
-    
-
-    
-        
-    
-
